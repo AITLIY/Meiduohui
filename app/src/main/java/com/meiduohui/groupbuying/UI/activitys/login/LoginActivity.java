360441e37696a5177ea0daf8f9c199d41d976c6c
AITLIY <873725468@qq.com>
2019-09-23 00:18:11 +0800

    添加登录注册界面；

---
+++ app/src/main/java/com/meiduohui/groupbuying/UI/activitys/login/LoginActivity.java
@@ -0,0 +1,155 @@
+package com.meiduohui.groupbuying.UI.activitys.login;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.lidroid.xutils.util.LogUtils;
+import com.meiduohui.groupbuying.R;
+import com.meiduohui.groupbuying.UI.activitys.HomepageActivity;
+import com.meiduohui.groupbuying.application.GlobalParameterApplication;
+import com.meiduohui.groupbuying.utils.ToastUtil;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private Context mContext;
+    private RequestQueue requestQueue;
+    private EditText username_ed,password_ed;
+    private LinearLayout login_ll,weixin_login_ll;
+    private TextView forget_password,register_new_user;
+    private LinearLayout ll_goto_register;
+
+    private static final int LOAD_DATA_SUCCESS = 101;
+    private static final int LOAD_DATA_FAILE1 = 102;
+    private static final int LOAD_DATA_FAILE2 = 103;
+    private static final int NET_ERROR = 404;
+
+    @SuppressLint("HandlerLeak")
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            switch (msg.what) {
+
+                case LOAD_DATA_SUCCESS:
+
+                    ToastUtil.show(mContext, "登录成功");
+                    GlobalParameterApplication.getInstance().setLoginStatus(true);
+                    startActivity(new Intent(mContext, HomepageActivity.class));
+                    finish();
+                    break;
+
+                case LOAD_DATA_FAILE1:
+
+                    String text = (String) msg.obj;
+                    LogUtils.i("LoginActivity: text " + text);
+                    ToastUtil.show(mContext, text);
+                    break;
+
+                case LOAD_DATA_FAILE2:
+
+                    ToastUtil.show(mContext, "登录失败");
+                    break;
+
+                case NET_ERROR:
+
+                    ToastUtil.show(mContext, "网络异常,请稍后重试");
+                    break;
+
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mContext = this;
+        requestQueue = GlobalParameterApplication.getInstance().getRequestQueue();
+        init();
+    }
+
+    private void init() {
+        initView();
+        initListner();
+    }
+
+
+    private void initView() {
+
+//        findViewById(R.id.iv_back).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                finish();
+//            }
+//        });
+
+        ll_goto_register = findViewById(R.id.ll_goto_register);
+
+    }
+
+    private void initListner() {
+
+        ll_goto_register.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+//            case R.id.ll_goto_login:
+//
+//                if (!NetworkUtils.isConnected(mContext)){
+//                    ToastUtil.show(mContext,"当前无网络");
+//                    return;
+//                }
+//
+//                String userName = username_ed.getText().toString();
+//                String passWord = password_ed.getText().toString();
+//
+//                if ("".equals(userName)||"".equals(passWord)) {
+//
+//                    ToastUtil.show(mContext,"用户账号或密码不能为空");
+//                    return;
+//                }
+//
+//                if (passWord.length() < 6 || passWord.length() > 12) {
+//
+//                    ToastUtil.show(mContext,"密码长度应为6~12位");
+//                    return;
+//                }
+//
+//                userLogin(userName, passWord);
+//
+//                break;
+
+//            case R.id.weixin_login_ll:
+//
+//                wxLogin();
+//                break;
+//
+//            case R.id.forget_password:
+//
+//                startActivity(new Intent(mContext, ForgetPasswordActivity1.class));
+//                break;
+//
+            case R.id.ll_goto_register:
+
+                startActivity(new Intent(mContext, RegisterActivity.class));
+                break;
+        }
+    }
+}
--- app/src/main/java/com/meiduohui/groupbuying/UI/fragments/MineFragment.java
+++ app/src/main/java/com/meiduohui/groupbuying/UI/fragments/MineFragment.java
@@ -2,14 +2,17 @@ package com.meiduohui.groupbuying.UI.fragments;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.android.volley.RequestQueue;
 import com.meiduohui.groupbuying.R;
+import com.meiduohui.groupbuying.UI.activitys.login.LoginActivity;
 import com.meiduohui.groupbuying.application.GlobalParameterApplication;
 
 /**
@@ -22,6 +25,7 @@ public class MineFragment extends Fragment implements View.OnClickListener {
     private View mView;
     private Context mContext;
     private RequestQueue requestQueue;
+    private ImageView iv_user_photo;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,17 +44,19 @@ public class MineFragment extends Fragment implements View.OnClickListener {
 
     private void initView() {
 
-//        ll_select_region = mView.findViewById(R.id.ll_select_region);
-//
-//        ll_select_region.setOnClickListener(this);
+        iv_user_photo = mView.findViewById(R.id.iv_user_photo);
+
+        iv_user_photo.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
 
         switch (v.getId()) {
-            case R.id.ll_select_region:
 
+            case R.id.iv_user_photo:
+
+                startActivity(new Intent(getContext(), LoginActivity.class));
                 break;
 
             case R.id.iv_scan_code:
